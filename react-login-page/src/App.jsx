3661c936cbd3e5e3c82cf60549c95acae38826be
Ciolpan Sergiu <ciolpan.sergiu64@gmail.com>
2022-11-14 21:27:42 +0200

    create AfterLoginPage component

--- react-login-page/src/App.jsx
+++ react-login-page/src/App.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import AfterLoginPage from "./components/AfterLoginPage";
 import CreateAccountPage from "./components/CreateAccountPage";
 import LoginPage from "./components/LoginPage";
 
 export default function App() {
 
-  
+  const [currentPage, setCurrentPage] = React.useState(
+    {
+      singinPage: true,
+      loginPage: false,
+      afterLoginPage: false
+    }
+  )
 
   return(
     <div>
-      <LoginPage />
+      {
+        currentPage.singinPage &&
+          <CreateAccountPage />
+      }
+      {
+        currentPage.loginPage &&
+        <LoginPage />
+      }
+      {
+        currentPage.afterLoginPage &&
+        <AfterLoginPage />
+      }
     </div>
   )
 }
---
+++ react-login-page/src/components/AfterLoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AfterLoginPage() {
+    return(
+        <div className="container">
+            <h2 className="centered-text container-main-header">Welcome to AfterLoginPage!</h2>
+            <button className="prev-page-btn">Back</button>
+            <button className="delete-account-btn">Delete Account</button>
+        </div>
+    )
+}
